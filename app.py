58076c66847dba9839678fc972abe2b77dc79960
germanavaz <gmsvaz@gmail.com>
2025-03-24 14:27:22 -0300

    :sparkles: Feat: Adicionado método DELETE

--- app.py
+++ app.py
@@ -67,5 +67,19 @@ def listar_livros():
     return jsonify(livros_formatados)
 
 
+@app.route('/livros/<int:livro_id>', methods=['DELETE'])
+def deletar_livro(livro_id):
+
+    with sqlite3.connect('database.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM livros WHERE id = ?", (livro_id,))
+        conn.commit()
+
+    if cursor.rowcount == 0:
+        return jsonify({"erro": "Livro não encontrado"}), 400
+
+    return jsonify({"menssagem": "Livro excluído com sucesso"}), 200
+
+
 if __name__ == '__main__':
     app.run(debug=True)
